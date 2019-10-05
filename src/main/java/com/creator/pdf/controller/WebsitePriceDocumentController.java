6b84303b6cee1b0dd609774f390d90e8c97304ce
Alina Shokota <alina.shokota159@gmail.com>
2019-10-05 16:21:01 +0200

    delete comments

--- src/main/java/com/creator/pdf/controller/WebsitePriceDocumentController.java
+++ src/main/java/com/creator/pdf/controller/WebsitePriceDocumentController.java
@@ -1,6 +1,5 @@
 package com.creator.pdf.controller;
 
-import com.creator.pdf.model.PriceRow;
 import com.creator.pdf.model.WebsitePriceDocument;
 import com.creator.pdf.pdf.PdfCreator;
 import com.creator.pdf.service.DocumentService;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
@@ -39,31 +37,6 @@ public class WebsitePriceDocumentController {
         return ResponseEntity.ok(websitePriceDocument);
     }
 
-
-//    @RequestMapping(value = "/save", method = RequestMethod.GET)
-//    public void save() {
-//        PriceRow priceRow = new PriceRow("A", "u", 23);
-//        PriceRow priceRow2 = new PriceRow("A", "a", 22);
-//        PriceRow priceRow3 = new PriceRow("F", "a", 23);
-//        PriceRow priceRow4 = new PriceRow("A", "t", 28);
-//        PriceRow priceRow5 = new PriceRow("A", "a", 23);
-//        PriceRow priceRow6 = new PriceRow("F", "a", 63);
-//        PriceRow priceRow7 = new PriceRow("A", "w", 23);
-//        PriceRow priceRow8 = new PriceRow("A", "a", 33);
-//        PriceRow priceRow9 = new PriceRow("F", "a", 13);
-//        List<PriceRow> priceRows = new ArrayList<>();
-//        priceRows.add(priceRow);
-//        priceRows.add(priceRow2);
-//        priceRows.add(priceRow3);
-//        priceRows.add(priceRow4);
-//        priceRows.add(priceRow5);
-//        priceRows.add(priceRow6);
-//        priceRows.add(priceRow7);
-//        priceRows.add(priceRow8);
-//        priceRows.add(priceRow9);
-//        WebsitePriceDocument websitePriceDocument = new WebsitePriceDocument("AAAA", "AAAA", priceRows);
-//        websitePriceDocumentServiceImpl.createDocument(websitePriceDocument);
-//    }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public ResponseEntity<?> addReport(@RequestBody WebsitePriceDocument document) {
